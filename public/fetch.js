572af341face3155517ba9bb4167ff4677aec034
BCox11 <blakecox.college@gmail.com>
2021-08-02 18:13:38 +0000

    Snoowrap loaded

--- public/fetch.js
+++ public/fetch.js
@@ -1,3 +1,12 @@
-var snoowrap = require('snoowrap');
+const r = new snoowrap({
+  userAgent: 'put your user-agent string here',
+  clientId: 'put your client id here',
+  clientSecret: 'put your client secret here',
+  refreshToken: 'put your refresh token here'
+});
 
-r.getNew('skyrimrequiem').then(console.log);
+
+// var snoowrap = require('snoowrap/src/snoowrap.js');
+/*require(['snoowrap/src/snoowrap.js'], function (snoowrap) {
+    //snoowrap is now loaded.
+}); */
