9f9759a48d9d645810215114da267f3c70c2f7c0
BCox11 <blakecox.college@gmail.com>
2021-08-03 17:20:15 +0000

    Can make requests; working refresh token

--- public/js/fetch.js
+++ public/js/fetch.js
@@ -1,13 +1,2 @@
-const r = new snoowrap({
-  userAgent: 'put your user-agent string here',
-  clientId: 'put your client id here',
-  clientSecret: 'put your client secret here',
-  refreshToken: 'put your refresh token here'
-});
+const r = new snoowrap(snoo);
 
-r.getNew().then(console.log);
-
-// var snoowrap = require('snoowrap/src/snoowrap.js');
-/*require(['snoowrap/src/snoowrap.js'], function (snoowrap) {
-    //snoowrap is now loaded.
-}); */
