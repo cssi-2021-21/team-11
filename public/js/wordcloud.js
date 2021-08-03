6ce2aee72b53c6d88ed4018e40635b7b2a7eca47
TiloRC <tiloreneau@gmail.com>
2021-08-03 18:29:19 +0000

    added test list2

--- public/js/wordcloud.js
+++ public/js/wordcloud.js
@@ -1,8 +1,10 @@
 
 
-list = [['foo', 60], ['bar', 30]]
+list1 = [['canvas1', 90],['foo', 60], ['bar', 30]]
+list2 = [['canvas2',90],['foo', 30], ['bar', 60]]
 
 createWordCloud = (list) => {
+    console.log("Running create word cloud")
     WordCloud(document.getElementById('my_canvas'), { list: list } );
 }
-createWrodCloud(list)
+createWordCloud(list1)
