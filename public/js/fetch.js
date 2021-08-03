17fdab4d2bbb34f6f8dd0eae6cafa153d71bc293
BCox11 <blakecox.college@gmail.com>
2021-08-03 18:06:59 +0000

    Fetching from titles implemented

--- public/js/fetch.js
+++ public/js/fetch.js
@@ -1,2 +1,23 @@
 const r = new snoowrap(snoo);
 
+
+
+const fetch = (word, sub) => {
+  let total = 0;
+  let count = 0;
+  newPosts = r.getNew(sub);
+  newPosts.forEach(post => {
+    postTitle = post.title.split(" ");
+    postTitle.forEach(currWord => {
+      total += 1;
+      if (currWord.toLowerCase() == word.toLowerCase()) {
+        count += 1;
+      }
+    });
+  }).then(() => {
+    console.log("Final Ratio: ")
+    console.log(count / total);
+  })
+}
+
+fetch("How", "redditdev");
