94d6170f5e6464b761b9afd3c24de23c2bdc56fe
TiloRC <tiloreneau@gmail.com>
2021-08-04 18:36:40 +0000

    sorts words by count and outputs results as list to search.html

--- public/js/count.js
+++ public/js/count.js
@@ -1,4 +1,4 @@
-testList = ["word1","word2","word3","word4","word1","word1"]
+testList = ["word1","word2","word3","word4","word1","word1","word4" ]
 
 count = (list) => {
     words = {}
@@ -12,4 +12,28 @@ count = (list) => {
     return words
 }
 
-console.log(count(testList))
+const sortWordsByFrequency = (words) => {
+    // items is words in 2d array form
+    let items = Object.keys(words).map(function(key) {
+        return [key, words[key]];
+    });
+    // Sort the array based on the second element - count
+    items.sort(function(first, second) {
+        return second[1] - first[1];
+    });
+    return items
+}
+
+const wordCounts = sortWordsByFrequency(count(testList))
+
+results = document.querySelector("#results")
+for (let i in wordCounts) {
+    const word = wordCounts[i][0]
+    const count = wordCounts[i][1]
+    console.log(word,": ",count)
+    const node = document.createElement("LI");    
+    const textnode = document.createTextNode(`${word}: ${count} (${100*count/testList.length}%)`);         // Create a text node
+    node.appendChild(textnode);         
+    results.appendChild(node);
+}
+
